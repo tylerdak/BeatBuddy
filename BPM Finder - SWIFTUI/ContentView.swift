b5e123827d789c9b79228dc60701310ebccf691d
tylerdak <dakint@icloud.com>
2020-06-01 11:20:12 -0400

    Update README.md

--- BPM Finder - SWIFTUI/ContentView.swift
+++ BPM Finder - SWIFTUI/ContentView.swift
@@ -9,8 +9,87 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var bpm: Double = 0.0
+    @State var lastTime: Double? = nil
+    let mainInitTitle = "Beat Buddy"
+    @State var title = "Beat Buddy"
+    var bpmSum: Double {
+        var total: Double = 0
+        for num in bpms {
+            total += num
+        }
+        return total
+    }
+    let leftImage = UIImage(named: "left")?.withRenderingMode(.alwaysTemplate)
+    let rightImage = UIImage(named: "right")?.withRenderingMode(.alwaysTemplate)
+    let waitImage = UIImage(named: "wait")?.withRenderingMode(.alwaysTemplate)
+    @State var currentImage = UIImage(named: "wait")?.withRenderingMode(.alwaysTemplate)
+
+    var bpmCount: Double {
+        return Double(bpms.count)
+    }
+    @State var bpms = [Double]()
+    let secondsInMinute: Double = 60
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("\(title)")
+            .font(.system(size: 50))
+            .fontWeight(.heavy)
+            .foregroundColor(Color(UIColor.label))
+            .padding(.all, 50)
+            Image(uiImage: currentImage!)
+                .resizable()
+                .aspectRatio(1, contentMode: .fit)
+                .foregroundColor(Color(UIColor.label))
+                .padding(.vertical,0)
+            Button(action: {
+                let timeSent: Double = CACurrentMediaTime()
+                if self.lastTime != nil {
+                    let difference = timeSent - self.lastTime!
+                    let bpm: Double = self.secondsInMinute/difference
+                    self.bpms.append(bpm)
+                    if self.bpmCount > 15 {
+                        self.bpms.removeFirst()
+                    }
+                    self.bpm = self.bpmSum/self.bpmCount
+                    self.title = "\(Int(self.bpm))"
+                }
+                self.lastTime = timeSent
+                if (self.currentImage == self.leftImage) {
+                    self.currentImage = self.rightImage
+                }
+                else {
+                    self.currentImage = self.leftImage
+                }
+            }) {
+                Text("Tap to the beat.")
+                    .font(.system(size: 18))
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color(UIColor.label))
+                    .padding(.all)
+            }
+            .background(Color(UIColor.secondarySystemFill))
+            .aspectRatio((16.0/9.0), contentMode: .fill)
+            .cornerRadius(15)
+            .padding(.all, 25)
+            
+            Button(action: {
+                self.lastTime = nil
+                self.bpms.removeAll()
+                self.title = self.mainInitTitle
+                self.currentImage = self.waitImage
+            }) {
+                Text("Reset")
+                    .font(.system(size: 14))
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color(UIColor.label))
+                    .padding(.all)
+            }
+            .background(Color(UIColor.secondarySystemFill))
+            .aspectRatio((16.0/9.0), contentMode: .fill)
+            .cornerRadius(15)
+            .padding(.all, 25)
+        }
     }
 }
 
---
+++ README.md
@@ -0,0 +1,15 @@
+# Beat Buddy
+Beat Buddy is a tool to help find beats per minute (BPM). This app is very simple and I do not intend to expand upon it at this time.
+
+## Description
+I've enabled Mac deployment for this app through Project Catalyst and included app icons for all supported platforms. As of writing this, landscape is enabled but I will disable that setting in the next commit.
+
+The "Beat Buddy" name is inspired by the character in the middle of the screen that dances along as you tap to the beat to find the BPM of your song.
+
+![Beat Buddy waiting to dance.](wait.png)
+This is Beat Buddy.
+
+---
+As previously stated, I do not intend to continue development on this project. This is just a silly project that I wanted to create but it may serve some use, both for learning and for using. I may add an ipa file and app file to the releases section in the future.
+
+As with all of my repos, if you'd like to return to this page, you can use [tylerdakin.com/BeatBuddy](https://tylerdakin.com/BeatBuddy) to return or [tylerdakin.com](https://tylerdakin.com) to view my profile.
